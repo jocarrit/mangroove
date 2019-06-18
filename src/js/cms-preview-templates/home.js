c3cc5e9dc4b12e99e2695c2f1be9bfaf11d65b3d
unknown <josemanuelcarrillo.dev@gmail.com>
2019-06-18 00:59:21 -0600

    traduccion de items

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -36,7 +36,7 @@ export default class PostPreview extends React.Component {
             </div>
 
             <div className="tc">
-              <a href="#" className="btn raise">See all products</a>
+              <a href="#" className="btn raise">+ Servicios</a>
             </div>
           </div>
         </div>
@@ -57,7 +57,7 @@ export default class PostPreview extends React.Component {
             </div>
 
             <div className="tc">
-              <a href="{{.buttonLink}}" className="btn raise">Read more</a>
+              <a href="{{.buttonLink}}" className="btn raise">Leer Mas</a>
             </div>
 
           </div>
